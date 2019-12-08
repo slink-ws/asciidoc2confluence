98f161283c395d2f06f4e3b0bbced25095ef0db3
Mikhail Kantur <kami@slink.ws>
2019-12-08 21:38:48 +1000

    AppConfig comments

--- src/main/java/ws/slink/config/AppConfig.java
+++ src/main/java/ws/slink/config/AppConfig.java
@@ -7,6 +7,23 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * accepts following configuration parameters from any spring-supported configuration source
+ * (properties file, command line arguments, environment variables):
+ *
+ *   a2c.url   - confluence server URL
+ *   a2c.user  - confluence server user
+ *   a2c.pass  - confluence server password
+ *   a2c.input - input file for processing
+ *   a2c.dir   - input directory for processing
+ *   a2c.space - confluence space override
+ *   a2c.clean - CSV list of confluence spaces to be cleaned up
+ *   a2c.force - perform forced clean up (remove all including 'protected' pages)
+ *   a2c.debug - print document to stdout in case of processing error
+ *
+ */
+
 @Data
 @Component
 @ConfigurationProperties(prefix = "a2c")
