ccb0e609f55f8b0cc87e52738acd572be2551552
Mikhail Kantur <kami@slink.ws>
2019-12-21 21:53:38 +1000

    EN-857: fix code block processing

--- src/main/java/com/dxfeed/config/AppConfig.java
+++ src/main/java/com/dxfeed/config/AppConfig.java
@@ -1,4 +1,4 @@
-package ws.slink.config;
+package com.dxfeed.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
--- src/main/java/com/dxfeed/tools/FluentJson.java
+++ src/main/java/com/dxfeed/tools/FluentJson.java
@@ -1,4 +1,4 @@
-package ws.slink.tools;
+package com.dxfeed.tools;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
